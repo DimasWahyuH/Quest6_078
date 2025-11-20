a074957b3063c9e5cd4568870b463fe2b530ee33
dimas <dimaswh19@gmail.com>
2025-11-20 14:53:36 +0700

    Menambahkan implementasi pemilihan jenis kelamin dengan RadioButton

--- app/src/main/java/com/example/quest6_078/view/Formulir.kt
+++ app/src/main/java/com/example/quest6_078/view/Formulir.kt
@@ -76,3 +76,28 @@ fun FormIsian(
                         txtNama = it
                     },
                 )
+                HorizontalDivider(
+                    modifier = Modifier
+                        .padding(20.dp)
+                        .width(250.dp), thickness = Thickness, color =
+                        Color.Red
+                )
+                Row {
+                    pilihanJK.forEach { item ->
+                        Row(modifier = Modifier.selectable(
+                            selected = txtGender == item,
+                            onClick = {
+                                txtGender = item
+                            }
+                        ),
+                            verticalAlignment = Alignment.CenterVertically) {
+                            RadioButton(
+                                selected = txtGender == item,
+                                onClick = {
+                                    txtGender = item
+                                }
+                            )
+                            Text(item)
+                        }
+                    }
+                }
